8f3161121d89fddc130c768414bcc0cdf5769d2d
sempeck <jarek.sepek@mediorama.pl>
2015-12-09 10:08:26 +0100

    Zadanie 1

---
+++ zad1.pas
@@ -0,0 +1,35 @@
+program sortowanie;
+
+var
+  tablica : array[1..50] of Char;
+  i,j : Integer;
+  temp : Char;
+
+procedure sort();
+  begin
+	for i:=1 to 49 do 
+     for j:=i+1 to 50 do 
+       begin
+       	if tablica[i]>tablica[j] then 
+       	  begin
+       	  	temp:=tablica[i];
+       	  	tablica[i]:=tablica[j];
+       	  	tablica[j]:=temp;
+       	  end;
+       end;
+  end;
+
+begin
+  
+  randomize;    
+
+  for i:=1 to 50 do 
+    tablica[i] := chr(random(26)+97);
+
+  sort();
+
+// pisanie
+  for i:=1 to 50 do 
+    writeln(tablica[i]);
+
+end.
